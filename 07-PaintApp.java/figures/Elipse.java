b15785c8227fceadad8c76a41a52a25a50096b35
Wesley Henrique <57545681+Wesley-WS@users.noreply.github.com>
2022-02-21 20:35:20 -0300

    Add files via upload

---
+++ 07-PaintApp.java/figures/Elipse.java
@@ -0,0 +1,48 @@
+package figures;
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class Elipse {
+    private int largura, altura;
+	private int x, y;
+	private Color cor;
+	
+	public Elipse( int x, int y, int largura, int altura){
+		this.x = x;
+		this.y = y;
+		this.largura = largura;
+		this.altura = altura;
+	}
+	
+	public void print(){
+		System.out.format("Elipse na posicao (%d, %d) tendo %d de largura e %d de altura.\n", this.x, this.y, this.largura, this.altura);
+	}
+	
+	public int area(){
+		int area = this.largura*this.altura*3;
+		return area;
+ }
+ 
+	public void drag (int dx, int dy){
+		this.x = this.x + dx;
+		this.y = this.y + dy;
+		
+ }
+	public void paint(Graphics g, boolean preenchido ){
+		Graphics2D g2d = (Graphics2D) g;
+		g.setColor(cor);
+		if(preenchido){
+			g2d.fillOval(this.x, this.y, this.largura, this.altura);
+		}else{
+			g2d.drawOval(this.x, this.y, this.largura, this.altura);
+		}
+	}
+	
+	public void setCor(Color cor){
+		this.cor = cor;
+	}
+	
+ 
+}
---
+++ 07-PaintApp.java/figures/Retangulo.java
@@ -0,0 +1,46 @@
+package figures;
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class Retangulo {
+    private int largura, altura;
+	private int x, y;
+	private Color cor;
+	
+	public Retangulo( int x, int y, int ladoA, int ladoB){
+		this.x = x;
+		this.y = y;
+		this.largura = largura;
+		this.altura = altura;
+	}
+	
+	public void print(){
+		System.out.format("Retangulo na posicao (%d, %d) com o ladoA tendo %d de largura e o ladoB tendo %d de altura.\n", this.x, this.y, this.largura, this.altura);
+	}
+	
+	public int area(){
+		int area = largura*altura;
+		return area;
+	}
+ 
+	public void drag (int dx, int dy){
+		this.x = this.x + dx;
+		this.y = this.y + dy;
+	}
+ 
+    public void paint(Graphics g, boolean preenchido ){
+		Graphics2D g2d = (Graphics2D) g;
+		g.setColor(cor);
+		if(preenchido){
+			g2d.fillRect(this.x, this.y, this.largura, this.altura);
+		}else{
+			g2d.drawRect(this.x, this.y, this.largura, this.altura);
+		}
+	}
+	
+	public void setCor(Color cor){
+		this.cor = cor;
+	}
+}
