bf24a16e0785601bbb903b7d76a3d29f412ad643
Wesley Henrique <57545681+Wesley-WS@users.noreply.github.com>
2022-02-23 20:23:09 -0300

    Update PaintApp.java

--- 07-PaintApp.java/PaintApp.java
+++ 07-PaintApp.java/PaintApp.java
@@ -8,7 +8,6 @@ class PaintApp{
 	
 	public static void main (String[] args){
 		PaintFrame frame = new PaintFrame();
-		frame.setVisible(true);
 	}
 }
 
@@ -17,11 +16,21 @@ class PaintFrame extends JFrame{
 	public Retangulo r1;
 	public Elipse e1;
 	
-	public void PaintFrame(){
+	PaintFrame(){
+		
+		this.addWindowListener (
+            new WindowAdapter() {
+                public void windowClosing (WindowEvent e) {
+                    System.exit(0);
+                }
+            }
+        );
+		
 		this.setTitle("Painting Figures");
 		this.setSize(350,350);
-		this.c1 = new Circulo(50,50,100);
+		this.setVisible(true);
 		
+		this.c1 = new Circulo(50,50,100);
 		this.r1 = new Retangulo(100,100,100,30);
 		this.e1 = new Elipse(150,150,100,30);
 		
@@ -33,7 +42,7 @@ class PaintFrame extends JFrame{
 		
 		this.r1.setCor(new Color(128,0,0));
 		this.r1.paint(g, false);
-		//this.r1.drag(50,50);
+		
 		this.e1.setCor(new Color(75,0,130));
 		this.e1.paint(g, true);
 		
